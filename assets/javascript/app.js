48369b46dec8704f59beef8cca1662b3a72ba431
Brock Belmonte <brockbel@gmail.com>
2018-03-06 21:01:33 -0600

    User added buttons now function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,50 +3,52 @@ var cars = ["McLaren", "Lamborghini", "Audi", "Bugatti", "Porsche", "Pagani", "K
 
 // Displays the Buttons on the page
 function renderButtons() {
-// Delete all of the car buttons prior to adding new ones to avoid a repeat
+  // Delete all of the car buttons prior to adding new ones to avoid a repeat
   $("#carContainer").empty();
-// Looping through the array of cars/buttons
+  // Looping through the array of cars/buttons
   for (var i = 0; i < cars.length; i++) {
-// Dynamically creates a button for each array element
+    // Dynamically creates a button for each array element
     var a = $("<button>");
-// Adding the class to the buttons
+    // Adding the class to the buttons
     a.addClass("car");
-// Adds a data-attribute with the value of the car name at the index of i
+    // Adds a data-attribute with the value of the car name at the index of i
     a.attr("data-name", cars[i]);
-// Putting the text on the button by taking the string from the array and using .text()
+    // Putting the text on the button by taking the string from the array and using .text()
     a.text(cars[i]);
-// Appending the button to the HTML page (Puts it on the end)
+    // Appending the button to the HTML page (Puts it on the end)
     $("#carContainer").append(a);
+    btnListener();
   }
 }
 
 // This function detects the click of the addCar button and adds a new element to the array
 $("#addCar").on("click", function(e) {
-
+  
   e.preventDefault();
-// Grab the input text value, trim extra spaces off beginning and end
+  // Grab the input text value, trim extra spaces off beginning and end
   var car = $("#carInput").val().trim();
-// Add the value to the array
+  // Add the value to the array
   cars.push(car);
   renderButtons();
 });
 
 renderButtons();
 
-$("button").on("click", function() {
-  // TODO: Grabbing and storing the array element value from the button being clicked
-  // This now works for all existing buttons on the page, it does work for buttons added by user...
-  var btnPressed = $(this).text();
-
-  // Constructing a queryURL using the car name
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+function btnListener() {
+  $("button").on("click", function() {
+    // TODO: Grabbing and storing the array element value from the button being clicked
+    // This now works for all existing buttons on the page, it does work for buttons added by user...
+    var btnPressed = $(this).text();
+    
+    // Constructing a queryURL using the car name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     btnPressed + "&api_key=QAKj5BHpQWrw3yXBgL0Tlk3WZ3dFVBVA&limit=10";
-
-  // Performing an AJAX request with the queryURL
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
+    
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
     // After data comes back from the request
     .then(function(response) {
       console.log(queryURL);
@@ -70,8 +72,9 @@ $("button").on("click", function() {
         $("#giphyDisplay").prepend(carDiv);
       }
     });
-});
-
-// TODO: Make Giphys come on the page still and start/stop when clicked...
-
-// Write an if statement that will add and remove "_s" to the end of the image url to change if it is still or not
+  });
+};
+  
+  // TODO: Make Giphys come on the page still and start/stop when clicked...
+  
+  // Write an if statement that will add and remove "_s" to the end of the image url to change if it is still or not
